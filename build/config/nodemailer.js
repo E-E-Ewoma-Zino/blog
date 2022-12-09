6dfaecd2ce57a0d423bea85e01129bdcc6a0403d
E-E-Ewoma-Zino <eewoma75@gmail.com>
2022-12-09 15:04:41 +0100

    fuck

--- build/config/nodemailer.js
+++ build/config/nodemailer.js
@@ -5,18 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const transporter = nodemailer_1.default.createTransport({
-    port: 5006,
-    host: "localhost",
-    tls: {
-        rejectUnauthorized: false
-    }
-    // host: "cpanel-s251.web-hosting.com:2096",
-    // port: 465,
-    // secure: true,
-    // requireTLS: true,
-    // auth: {
-    // 	user: "info@global-finance-news.com",
-    // 	pass: "Admin1@global-finance-news.com"
+    // port: 5006, // Postfix uses port 25
+    // host: "localhost",
+    // tls: {
+    //   rejectUnauthorized: false
     // }
+    host: "cpanel-s251.web-hosting.com:2096",
+    port: 465,
+    secure: true,
+    requireTLS: true,
+    auth: {
+        user: "info@global-finance-news.com",
+        pass: "Admin1@global-finance-news.com"
+    }
 });
 exports.default = transporter;
--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,19 +1,19 @@
 import nodeMailer from "nodemailer";
 
 const transporter = nodeMailer.createTransport({
-	port: 5006, // Postfix uses port 25
-	host: "localhost",
-	tls: {
-	  rejectUnauthorized: false
-	}
-	// host: "cpanel-s251.web-hosting.com:2096",
-	// port: 465,
-	// secure: true,
-	// requireTLS: true,
-	// auth: {
-	// 	user: "info@global-finance-news.com",
-	// 	pass: "Admin1@global-finance-news.com"
+	// port: 5006, // Postfix uses port 25
+	// host: "localhost",
+	// tls: {
+	//   rejectUnauthorized: false
 	// }
+	host: "cpanel-s251.web-hosting.com:2096",
+	port: 465,
+	secure: true,
+	requireTLS: true,
+	auth: {
+		user: "info@global-finance-news.com",
+		pass: "Admin1@global-finance-news.com"
+	}
 });
 
 export default transporter;
